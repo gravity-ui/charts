4fca2545052afee70ecd02c123e6a51a31bc09f2
Irina Kuzmina <kuzmadom@yandex-team.ru>
2025-05-29 10:20:04 +0300

    chore: update screenshot

--- src/__tests__/treemap.visual.test.tsx
+++ src/__tests__/treemap.visual.test.tsx
@@ -6,7 +6,7 @@ import {treemapBasicData} from 'src/__stories__/__data__';
 
 import {ChartTestStory} from '../../playwright/components/ChartTestStory';
 
-test.describe.only('Treemap series', () => {
+test.describe('Treemap series', () => {
     test('Basic', async ({mount}) => {
         const component = await mount(<ChartTestStory data={treemapBasicData} />);
         await expect(component.locator('svg')).toHaveScreenshot();
