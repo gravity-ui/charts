30f6b6d15350975d44f89fb4e07ae362309db8cb
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2025-11-27 17:47:49 +0300

    chore: fix axis maxPadding description

--- src/types/chart/axis.ts
+++ src/types/chart/axis.ts
@@ -151,6 +151,7 @@ export interface ChartAxis {
     };
     /** Padding of the max value relative to the length of the axis.
      * A padding of 0.05 will make a 100px axis 5px longer.
+     * Does not apply for series with data normalization along on a specific axis (stacking: 'percent').
      * @default 0.05 for Y axis, 0.01 for X axis
      */
     maxPadding?: number;
