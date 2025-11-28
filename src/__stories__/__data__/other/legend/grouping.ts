055e14c820e13b91501aff52f05dce8d43f5eb85
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2025-11-28 11:17:52 +0300

    fix: review fixes

--- src/__stories__/__data__/other/legend/grouping.ts
+++ src/__stories__/__data__/other/legend/grouping.ts
@@ -18,8 +18,8 @@ export const groupedLegend: ChartData = {
                 data: [{x: 1.2, y: 1.2}],
                 legend: {groupId: 's1'},
             },
-            {type: 'scatter', name: 'Series 2.1', data: [{x: 2.1, y: 2.1}]},
-            {type: 'scatter', name: 'Series 2.2', data: [{x: 2.2, y: 2.2}]},
+            {type: 'scatter', name: 'Series 2', data: [{x: 2, y: 2}]},
+            {type: 'scatter', name: 'Series 3', data: [{x: 3, y: 3}]},
         ],
     },
 };
