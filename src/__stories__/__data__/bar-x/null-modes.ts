1de06953f0cc329ede14d5f8b576683ec71e4987
Denis Ryabko <ryabko.denis2014@yandex.com>
2025-12-06 13:23:03 +0300

    fix(BarX): null mode skip for barX category

--- src/__stories__/__data__/bar-x/null-modes.ts
+++ src/__stories__/__data__/bar-x/null-modes.ts
@@ -8,6 +8,14 @@ const dataWithNullsLinearX: BarXSeriesData[] = [
     {x: 30, y: 30},
 ];
 
+const dataWithNullsCategoryX: BarXSeriesData[] = [
+    {x: 0, y: 10},
+    {x: 1, y: 15},
+    {x: 2, y: null},
+    {x: 3, y: 25},
+    {x: 4, y: 30},
+];
+
 export const barXNullModeSkipLinearXData: ChartData = {
     series: {
         data: [
@@ -31,5 +39,41 @@ export const barXNullModeZeroLinearXData: ChartData = {
                 nullMode: 'zero',
             },
         ],
+    },
+};
+
+export const barXNullModeSkipCategoryXData: ChartData = {
+    series: {
+        data: [
+            {
+                type: 'bar-x',
+                name: 'Series',
+                data: dataWithNullsCategoryX,
+                nullMode: 'skip',
+                dataLabels: {enabled: true, inside: true},
+            },
+        ],
+    },
+    xAxis: {
+        type: 'category',
+        categories: ['A', 'B', 'C', 'D', 'E'],
+    },
+};
+
+export const barXNullModeZeroCategoryXData: ChartData = {
+    series: {
+        data: [
+            {
+                type: 'bar-x',
+                name: 'Series',
+                data: dataWithNullsCategoryX,
+                nullMode: 'zero',
+                dataLabels: {enabled: true, inside: true},
+            },
+        ],
+    },
+    xAxis: {
+        type: 'category',
+        categories: ['A', 'B', 'C', 'D', 'E'],
     },
 };
