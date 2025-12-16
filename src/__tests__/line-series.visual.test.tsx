b83858bc178517ee28bbbf0703879e8c849e1875
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2025-12-16 14:46:55 +0300

    chore: add line split test

--- src/__tests__/line-series.visual.test.tsx
+++ src/__tests__/line-series.visual.test.tsx
@@ -141,4 +141,39 @@ test.describe('Line series', () => {
             await expect(component.locator('svg')).toHaveScreenshot();
         });
     });
+
+    test('Basic split', async ({mount}) => {
+        const chartData: ChartData = {
+            series: {
+                data: [
+                    {
+                        type: 'line',
+                        name: 'Series 1',
+                        data: [
+                            {x: 1, y: 1},
+                            {x: 2, y: 2},
+                        ],
+                        yAxis: 0,
+                    },
+                    {
+                        type: 'line',
+                        name: 'Series 2',
+                        data: [
+                            {x: 2, y: 1},
+                            {x: 1, y: 2},
+                        ],
+                        yAxis: 1,
+                    },
+                ],
+            },
+            yAxis: [{plotIndex: 0}, {plotIndex: 1}],
+            split: {
+                enable: true,
+                plots: [{}, {}],
+                gap: 20,
+            },
+        };
+        const component = await mount(<ChartTestStory data={chartData} />);
+        await expect(component.locator('svg')).toHaveScreenshot();
+    });
 });
