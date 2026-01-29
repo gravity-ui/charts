ff5a73cdeac53411c00b31bf40e0f2b3e4a6b72e
Mr.Dr.Professor Patrick <alaev89@yandex-team.ru>
2026-01-29 12:13:11 +0100

    fix: fix vertical clipping path handling for line series (#387)

--- src/components/ChartInner/useChartInnerProps.ts
+++ src/components/ChartInner/useChartInnerProps.ts
@@ -210,6 +210,7 @@ export function useChartInnerProps(props: Props) {
         htmlLayout,
         clipPathId,
         isOutsideBounds,
+        zoomState,
     });
 
     const handleAttemptToSetZoomState = React.useCallback(
