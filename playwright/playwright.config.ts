4e859733a6a29c1144df88d6edcee0999b43df1f
Evgenii Alaev <alaev89@yandex-team.ru>
2026-02-26 23:44:25 +0100

    fix: add todo

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -40,6 +40,7 @@ const config: PlaywrightTestConfig = {
     forbidOnly: Boolean(process.env.CI),
     /* Retry on CI only */
     retries: process.env.CI ? 1 : 0,
+    // TODO: return 8 workers after https://github.com/gravity-ui/charts/issues/424
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 4 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
