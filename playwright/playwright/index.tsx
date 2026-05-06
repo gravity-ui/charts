134ce0912a3c14a5d3a5f036ac14f0297015a67d
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2026-05-06 13:23:21 +0300

    fix: fix playwright

--- playwright/playwright/index.tsx
+++ playwright/playwright/index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import {MobileProvider, ThemeProvider} from '@gravity-ui/uikit';
 import {beforeMount} from '@playwright/experimental-ct-react/hooks';
 
+import '../../src/plugins';
+
 import '@gravity-ui/uikit/styles/styles.scss';
 
 beforeMount(async ({App}) => {
