e2d467800bf16931aacd456194239176169c0627
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2026-08-14 15:50:05 +0300

    docs: add axis labels guide, examples, and tests

--- src/__tests__/docs-examples.visual.test.tsx
+++ src/__tests__/docs-examples.visual.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {expect, test} from '@playwright/experimental-ct-react';
 
+import {AxisLabelFontSizeExample} from '../../docs/examples/src/charts/axis-labels/font-size';
 import {CategoryAxisExample} from '../../docs/examples/src/charts/axis-types/category';
 import {DatetimeAxisExample} from '../../docs/examples/src/charts/axis-types/datetime';
 import {LinearAxisExample} from '../../docs/examples/src/charts/axis-types/linear';
@@ -12,6 +13,17 @@ const CONTAINER_STYLE: React.CSSProperties = {
     height: 320,
     display: 'inline-block',
 };
+
+test.describe('Docs examples: Axis Labels', () => {
+    test('custom font size', async ({mount}) => {
+        const component = await mount(
+            <div style={CONTAINER_STYLE}>
+                <AxisLabelFontSizeExample />
+            </div>,
+        );
+        await expect(component.locator('svg')).toBeVisible();
+    });
+});
 
 test.describe('Docs examples: Axis Types', () => {
     test('linear axis', async ({mount}) => {
